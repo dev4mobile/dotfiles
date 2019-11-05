ad424e2ff79063c692f5aab1e031e5aab851cfa5
dev4mobile <cn.dev4mobile@gmail.com>
2019-11-05 19:37:13 +0800

    update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -2,9 +2,23 @@
 
 set -e
 
+if [ -z $DEV ]; then
+  export DEV=$HOME/develop
+fi
+
 DOTFILE_DIR=$DEV/dotfiles
 
+if [ ! -d $DOTFILE_DIR ]; then
+  echo "dotfiles is not in $HOME/develop"
+  exit 2
+fi
+
+if [ ! -f $DOTFILE_DIR/bin/init.sh ]; then
+  echo "dotfiles init script is not exist"
+  exit 2
+fi
+
+ln -sf $DOTFILE_DIR/bin/init.sh /usr/local/bin/dotfiles
 ln -sf $DOTFILE_DIR/ssh/.ssh $HOME/.ssh
 ln -sf $DOTFILE_DIR/zsh/.zshrc $HOME/.zshrc
 ln -sf $DOTFILE_DIR/vim/.vimrc $HOME/.vimrc
-
