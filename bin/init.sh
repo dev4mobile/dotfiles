cdf72d43eac5bce4a10860d2fc5bba697b15801f
dev4mobile <cn.dev4mobile@gmail.com>
2019-11-06 19:48:50 +0800

    :tada: using for loop to link dot files

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -18,7 +18,23 @@ if [ ! -f $DOTFILE_DIR/bin/init.sh ]; then
   exit 2
 fi
 
-ln -sf $DOTFILE_DIR/bin/init.sh /usr/local/bin/dotfiles
-ln -sf $DOTFILE_DIR/ssh/.ssh $HOME/.ssh
-ln -sf $DOTFILE_DIR/zsh/.zshrc $HOME/.zshrc
-ln -sf $DOTFILE_DIR/vim/.vimrc $HOME/.vimrc
+
+#ln -sf $DOTFILE_DIR/bin/init.sh /usr/local/bin/dotfiles
+#ln -sf $DOTFILE_DIR/ssh/.ssh $HOME/.ssh
+#ln -sf $DOTFILE_DIR/zsh/.zshrc $HOME/.zshrc
+#ln -sf $DOTFILE_DIR/vim/.vimrc $HOME/.vimrc
+
+for file in `ls $DOTFILE_DIR`; do
+  if [[ $file = "iTerm2" || $file = "README.md" ]]; then
+   continue
+  else 
+    su_dir_parent=$DOTFILE_DIR/$file
+    for sub_dir in $(ls -A $su_dir_parent | egrep '^\.'); do
+      if [[ $sub_dir = ".gitignore" ]]; then
+        continue 
+      else 
+        `ln -sf $su_dir_parent/$sub_dir $HOME/$sub_dir`
+      fi   
+    done
+  fi
+done
